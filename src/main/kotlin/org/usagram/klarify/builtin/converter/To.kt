65ff111ee0cc5f4170883096a08d77b3ddad0c43
Takahiro Kondo <takahiro.kondo@bizreach.co.jp>
2022-01-23 13:46:34 +0900

    As -> To

--- src/main/kotlin/org/usagram/klarify/builtin/converter/To.kt
+++ src/main/kotlin/org/usagram/klarify/builtin/converter/To.kt
@@ -3,7 +3,7 @@ package org.usagram.klarify.builtin.converter
 import org.usagram.klarify.Output
 import org.usagram.klarify.converter.Converter
 
-class As<I, O>(
+class To<I, O>(
     private val convert: (I) -> Output<O>
 ) : Converter<I, O> {
 
@@ -11,5 +11,5 @@ class As<I, O>(
         convert(value)
 
     override fun toString(): String =
-        "As($convert)"
+        "To($convert)"
 }
