43b628dcda59384de13cc418da872bf47bc69848
Takahiro Kondo <takahiro.kondo@bizreach.co.jp>
2022-01-23 15:04:46 +0900

    Fix return value of to function

--- src/main/kotlin/org/usagram/klarify/dsl/generic/to.kt
+++ src/main/kotlin/org/usagram/klarify/dsl/generic/to.kt
@@ -2,7 +2,8 @@ package org.usagram.klarify.dsl.generic
 
 import org.usagram.klarify.Output
 import org.usagram.klarify.builtin.converter.To
+import org.usagram.klarify.converter.Converter
 import org.usagram.klarify.dsl.Dsl
 
-fun <I, O> Dsl<I>.to(convert: (I) -> Output<O>): To<I, O> =
+fun <I, O> Dsl<I>.to(convert: (I) -> Output<O>): Converter<I, O> =
     To(convert)
